75ba9d52fcef67de856a3297f85b1c81c1ae87b6
Tiziano Levi Martin Bernal <tmartinbernal@udesa.edu.ar>
2022-04-09 22:56:19 -0300

    Primer commit

---
+++ cambios 2.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  9 22:39:12 2022
+
+@author: Acer
+"""
+
+print("cosa loca")
---
+++ changes.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  9 21:28:50 2022
+
+@author: Acer
+"""
+print("hello world")
